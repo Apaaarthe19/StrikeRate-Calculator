44cf22d7bcd012b81d9f8324e95f59b2b531b3ce
PARTH SARTHI GUPTA <parthgupta19062004@gmail.com>
2025-09-29 11:25:48 +0530

    StrikeRate-Calculator.py

--- StrikeRate Calculator.py
+++ StrikeRate Calculator.py
@@ -2,11 +2,10 @@
 #Runs per 100 balls
 
 print("Welcome To The Batting Strike Rate Calculator created by Parth")
-user_name = input("Please enter the name of the Batsman:")
-r = input("Please enter the Runs scored by the Batsman:")
-b = input("Please enter the number of Balls played by the Batsman:")
-r = int(r)
-b = int(b)
-avg= r/b
-sr=avg*100
-print("The strike rate of", user_name, "is", sr)
+user_name = vars(input("Please enter the name of the Batsman:"))
+runs = int(input("Please enter the Runs scored by the Batsman:"))
+balls = int(input("Please enter the number of Balls played by the Batsman:"))
+avg= runs/balls 
+strike_rate=avg*100
+
+print("The strike rate of", user_name, "is", strike_rate)
